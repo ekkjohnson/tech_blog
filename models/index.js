def8ff55a9b6ef3fcaef516b56cc3648278143f9
Elizabeth <ekkjohnson@gmail.com>
2022-05-08 14:11:37 -0500

    posts

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const User = require('./user');
+const Post = require('./posts');
+const Comments = require('./comments');
+
+User.hasMany(Post, {
+    foreignKey: 'post_id',
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'post_id',
+});
+
+
+
+Post.hasMany(Comments, {
+    foreignKey: 'post_id',
+});
+
+
+
+Comments.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
+
+module.exports = { User, Post, Comments };
