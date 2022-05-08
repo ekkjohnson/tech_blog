e887512363b3d3d7bf6f2604d9f5e68ff6ea1cd1
Elizabeth <ekkjohnson@gmail.com>
2022-05-08 18:43:33 -0500

    index route

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const homeRoutes = require('./homeRoutes')
+const apiRoutes = require('./api')
+
+router.use('/home', homeRoutes)
+router.use('/api', apiRoutes)
+
+router.get('*', (req, res) => {
+    res.redirect('/home')
+})
+
+module.exports = router;
