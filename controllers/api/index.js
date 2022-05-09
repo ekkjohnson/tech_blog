99d7305bb7fb2c7c8849d1ae5fcd07b930591074
Elizabeth <ekkjohnson@gmail.com>
2022-05-09 18:06:54 -0500

    reworked homeroute

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const loginRoutes = require('./loginRoutes');
 // const messageRoutes = require('./postRoutes');
-const signupRoutes = require('./signupRoute');
+const signupRoutes = require('./signupRoutes');
 
 
 router.use('/login', loginRoutes);
