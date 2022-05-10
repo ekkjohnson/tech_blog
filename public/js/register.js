6d408729082be6884d4050b111a2f6c104d12a80
Elizabeth <ekkjohnson@gmail.com>
2022-05-09 22:16:41 -0500

    register

---
+++ public/js/register.js
@@ -0,0 +1,26 @@
+const registerFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector("#username-register").value.trim();
+    const password = document.querySelector("#password-register").value.trim();
+    const confirmPassword = document.querySelector("#confirm-register").value.trim()
+    console.log(username);
+  
+    if (username && password && password === confirmPassword) {
+      const response = await fetch("/api/user/register", {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        return document.location.replace("/api/recipes");
+      } else {
+        return console.error("Invalid Username or Password!");
+      }
+    }
+  };
+  
+  document
+    .querySelector(".register-form")
+    .addEventListener("submit", registerFormHandler);
