d1920879a2ced452d161d79bd3c023c7f244baac
Elizabeth <ekkjohnson@gmail.com>
2022-05-09 22:16:18 -0500

    new post

---
+++ public/js/newPost.js
@@ -0,0 +1,18 @@
+const addNewPost = async () => {
+    event.preventDefault();
+    const title = document.querySelector("#post-title-input").value.trim();
+    const content = document.querySelector("#post-content-input").value.trim();
+    if (title == "" || content == "") {
+      return alert("You must enter a title and content for your new post!");
+    } else {
+      const newPost = await fetch("/api/dashboard/add", {
+        method: "POST",
+        body: JSON.stringify({ title, content }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      return document.location.replace("/api/dashboard");
+    }
+  };
+  
+  document.querySelector(".new-post-form").addEventListener("submit", addNewPost);
