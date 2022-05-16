2aebfbeee9bb06068d76479029a08b40ecf54dc9
Elizabeth <ekkjohnson@gmail.com>
2022-05-16 10:13:41 -0500

    post route fix

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -61,5 +61,45 @@ router.get("/:id", withAuth, async (req, res) => {
       res.json(err);
     }
   });
+
+  router.put('/:id', async (req, res) => {
+    const updatedPost = await Post.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    console.log(updatedPost.user_id);
+    try {
+      if(!updatedPost) {
+        res.status(404).json({ message: 'Could not find post'})
+      }
+      if(updatedPost.id === req.session.id) {
+        await updatedPost.update({ content: req.body.content})
+      }
+  
+      res.json(updatedPost)
+  
+    } catch (err) {
+      res.json(err)
+    }
+  })
+  
+  router.delete('/:id', async (req, res) => {
+    try {
+      const deletedPost = await Post.findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+        if(updatedPost.user_id == req.session.id) {
+          await deletedPost.destroy()
+          res.json(deletedPost)
+        } else {
+          res.json({ message: 'You are not the owner of this post.'})
+        }
+    } catch (err) {
+      res.json(err)
+    }
+  })
   
 module.exports = router;
