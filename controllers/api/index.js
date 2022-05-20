9ea25ef467c6f95496f714c3a087648edfebd630
Elizabeth <ekkjohnson@gmail.com>
2022-05-20 10:17:27 -0500

    dash change

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const dashboardRoute = require('./dashboardRoute')
-const postRoute = require('./postRoute')
+
 const userRoute = require('./userRoute')
 
 router.use('/dashboard', dashboardRoute)
-router.use('/post', postRoute)
+
 router.use('/user', userRoute)
 
 
